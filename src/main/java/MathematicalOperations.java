a14da7dcfe791fc45df9689a92b0fdbaa870eaad
narinmahmuti <ninamah94@gmail.com>
2015-05-07 20:16:02 +0300

    Comment for the power operation added

--- src/main/java/MathematicalOperations.java
+++ src/main/java/MathematicalOperations.java
@@ -22,6 +22,12 @@ public class MathematicalOperations {
         return (float)(x)/(float)(y);
     }
 
+/**
+* Raises x to the power of n
+* @param x base
+* @param n exponent
+* @return exponentiation of x to the nth power (x^n)
+*/
     double power(double x, int n){
         if(n==0)
             return 1;
